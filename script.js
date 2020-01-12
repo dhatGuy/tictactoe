8547c5a6123b349480d3bd0b871bf431c34f9441
Moritz Hofmann <mowmoh@gmail.com>
2020-01-12 23:26:17 +0100

    Working Tic Tac Toe

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ const gameBoard = (() => {
             else if(board[g] === "O") amountofO++;
         }
 
+        console.log("amountofX " + amountofX);
+        console.log("amountofO " + amountofO);
+
+
         if (amountofX > amountofO) {
         e.target.textContent = 'O'
         board[e.target.id-1] = "O";
@@ -25,7 +29,17 @@ const gameBoard = (() => {
             e.target.textContent = 'X'
             board[e.target.id-1] = "X";
             amountofX++;
-            gameFlow.computerMove();
+            if(player2.name === "Computer (easy)") {
+                gameFlow.computerMove(); 
+            }
+            else if (player2.name === "Computer (unbeatable)") {
+                if((amountofO + amountofX) <= 8) {
+                let aiIndex = aiPlay();
+                document.getElementById(`${aiIndex+1}`).textContent = "O";
+                gameBoard.board[aiIndex] = "O";
+                }
+            }
+
         }
   if(amountofX === 5 && amountofO === 4) {
         gameFlow.checkWin("end");
@@ -42,21 +56,61 @@ function initializeGame() {
     }     
 };
 
- 
+
+ const playMode = () =>{
+     let pvp = document.querySelector('[value=PVP]')
+     let pvcEasy = document.querySelector('[value=PVC-easy]')
+     let pvcUnbeat = document.querySelector('[value=PVC-unbeatable]')
+     let players = document.querySelector('.player2')
+
+     if(pvcEasy.checked){
+        players.style.display = 'none';
+        document.getElementById('player2').value="Computer (easy)";
+     }
+
+     else if(pvcUnbeat.checked) {
+        players.style.display = 'none';
+        document.getElementById('player2').value="Computer (unbeatable)";  
+     }
+
+     else if(pvp.checked) {
+        document.getElementById('player2').value=""; 
+        players.style.display = 'block';
+
+     }
+
+ };
+
+ const startGame = () =>{
+    document.querySelector('form')
+     .addEventListener('submit', e => {
+         gameFlow.setPlayers()
+         e.preventDefault()
+     })
+ }
+
+
+
+
 
     let board = 
         [
-            "", "", "",
-            "", "", "",
-            "", "", ""
+            0, 1, 2,
+            3, 4, 5,
+            6, 7, 8
         ]
 
     return {
         board,
         clickFunction,
-        initializeGame
+        initializeGame,
+        playMode,
+        startGame
     }
 })();
+
+gameBoard.startGame();
+
 
 const Player = (name) => {
     let score = 0;
@@ -136,8 +190,9 @@ const gameFlow = (() => {
 
 
     let newGame = function() { 
+      //  gameBoard.board = [0,1,2,3,4,5,6,7,8];
         for(let g=0; g<gameBoard.board.length;g++) {
-            gameBoard.board[g] = "";
+            gameBoard.board[g] = g;
             document.getElementById(`${g+1}`).textContent = "";
         }
         gameBoard.initializeGame();
@@ -171,7 +226,7 @@ const gameFlow = (() => {
         let freeSpaces = 0;
 
         for(let g=0; g<gameBoard.board.length; g++) {
-            if(gameBoard.board[g] === "") freeSpaces++;
+            if(gameBoard.board[g] !== "O" && gameBoard.board[g] !== "X") freeSpaces++;
         }
 
         let atFreeSpace = 0;
@@ -179,7 +234,7 @@ const gameFlow = (() => {
         let newMove = Math.ceil(Math.random()*freeSpaces);
 
         for(let e=0; e<gameBoard.board.length; e++) {
-            if(gameBoard.board[e] === "") atFreeSpace++;
+            if(gameBoard.board[e] !== "O" && gameBoard.board[e] !== "X") atFreeSpace++;
             if(atFreeSpace === newMove) {
 
                 document.getElementById(`${e+1}`).textContent = "O";
@@ -203,3 +258,133 @@ const gameFlow = (() => {
         computerMove
     }
 })();
+
+
+function aiPlay() {
+
+    let origBoard = ["O",1,"X","X",4,"X",6,"O","O"];
+    let aiPlayer = "O";
+    let huPlayer = "X";
+
+    // winning combinations using the board indexies for instace the first win could be 3 xes in a row
+    function winning(board, player){
+        if (
+               (board[0] == player && board[1] == player && board[2] == player) ||
+               (board[3] == player && board[4] == player && board[5] == player) ||
+               (board[6] == player && board[7] == player && board[8] == player) ||
+               (board[0] == player && board[3] == player && board[6] == player) ||
+               (board[1] == player && board[4] == player && board[7] == player) ||
+               (board[2] == player && board[5] == player && board[8] == player) ||
+               (board[0] == player && board[4] == player && board[8] == player) ||
+               (board[2] == player && board[4] == player && board[6] == player)
+               ) {
+               return true;
+           } else {
+               return false;
+           }
+       }
+
+       // returns the available spots on the board
+    function emptyIndexies(board){
+    return  board.filter(s => s != "O" && s != "X");
+  }
+
+
+  // keep track of function calls
+var fc = 0;
+
+// finding the ultimate play on the game that favors the computer
+var bestSpot = minimax(gameBoard.board, aiPlayer);
+
+//logging the results
+console.log("index: " + bestSpot.index);
+console.log("function calls: " + fc);
+
+// the main minimax function
+function minimax(newBoard, player){
+  
+  //keep track of function calls;
+  fc++;
+
+  //available spots
+  var availSpots = emptyIndexies(newBoard);
+
+  // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+  if (winning(newBoard, huPlayer)){
+     return {score:-10};
+  }
+	else if (winning(newBoard, aiPlayer)){
+    return {score:10};
+	}
+  else if (availSpots.length === 0){
+  	return {score:0};
+  }
+
+// an array to collect all the objects
+  var moves = [];
+
+  // loop through available spots
+  for (var i = 0; i < availSpots.length; i++){
+    //create an object for each and store the index of that spot that was stored as a number in the object's index key
+    var move = {};
+  	move.index = newBoard[availSpots[i]];
+
+    // set the empty spot to the current player
+    newBoard[availSpots[i]] = player;
+
+    //if collect the score resulted from calling minimax on the opponent of the current player
+    if (player == aiPlayer){
+      var result = minimax(newBoard, huPlayer);
+      move.score = result.score;
+    }
+    else{
+      var result = minimax(newBoard, aiPlayer);
+      move.score = result.score;
+    }
+
+    //reset the spot to empty
+    newBoard[availSpots[i]] = move.index;
+
+    // push the object to the array
+    moves.push(move);
+  }
+
+// if it is the computer's turn loop over the moves and choose the move with the highest score
+  var bestMove;
+  if(player === aiPlayer){
+    var bestScore = -10000;
+    for(var i = 0; i < moves.length; i++){
+      if(moves[i].score > bestScore){
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }else{
+
+// else loop over the moves and choose the move with the lowest score
+    var bestScore = 10000;
+    for(var i = 0; i < moves.length; i++){
+      if(moves[i].score < bestScore){
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+// return the chosen move (object) from the array to the higher depth
+  return moves[bestMove];
+}
+
+return bestSpot.index;
+
+
+};
+
+const startGame = () =>{
+     document.querySelector('form')
+     .addEventListener('submit', e => {
+         gameFlow.setPlayers()
+         e.preventDefault()
+     })
+ };
+ startGame();
